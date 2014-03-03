f1a37bb445f898ffd4153b93fb5ff7c5bb8fd2fa
Ian Compton <ian.compton@dc-storm.com>
2014-03-03 10:23:16 +0000

    Updated Cask file, added in Go mode hook for flycheck

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,7 +1,7 @@
 (require 'go-mode-load)
 (require 'go-autocomplete)
 (require 'auto-complete-config)
-
+(add-hook 'go-mode-hook 'flycheck-mode)
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (provide 'init-go)
