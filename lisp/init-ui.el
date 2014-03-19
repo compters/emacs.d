e82c10f8548f26d5427e5c431a08643d0b1df195
Ian Compton <ian.a.compton@googlemail.com>
2014-03-19 18:49:07 +0000

    Fixed bad copy paste error

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,7 +1,7 @@
-(require 'framemove)                                                                                                                  |
-(global-set-key (kbd "C-c <left>")  'windmove-left)                                                                                   |
-(global-set-key (kbd "C-c <right>") 'windmove-right)                                                                                  |
-(global-set-key (kbd "C-c <up>")    'windmove-up)                                                                                     |
+(require 'framemove)                                                                                                                  
+(global-set-key (kbd "C-c <left>")  'windmove-left)                                                                                   
+(global-set-key (kbd "C-c <right>") 'windmove-right)                                                                                  
+(global-set-key (kbd "C-c <up>")    'windmove-up)                                                                                     
 (global-set-key (kbd "C-c <down>")  'windmove-down)   
 
 (require 'powerline)
